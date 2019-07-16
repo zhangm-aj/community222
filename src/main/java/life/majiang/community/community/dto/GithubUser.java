74e6c4f11e460d9c2fc1680148f9be48452cc859
zhangm-aj <624638704@qq.com>
2019-07-16 18:15:29 +0800

    'second_commit1'

--- src/main/java/life/majiang/community/community/dto/GithubUser.java
+++ src/main/java/life/majiang/community/community/dto/GithubUser.java
@@ -28,6 +28,15 @@ public class GithubUser{
     public void setBio(String bio) {
         this.bio = bio;
     }
+
+    @Override
+    public String toString() {
+        return "GithubUser{" +
+                "name='" + name + '\'' +
+                ", id=" + id +
+                ", bio='" + bio + '\'' +
+                '}';
+    }
 }
 
 
