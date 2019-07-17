171411453693bd92073fe4bc22acc460142b01df
zhangm-aj <624638704@qq.com>
2019-07-17 16:50:13 +0800

    'denglu'

--- src/main/java/life/majiang/community/community/mapper/UserMapper.java
+++ src/main/java/life/majiang/community/community/mapper/UserMapper.java
@@ -3,9 +3,14 @@ package life.majiang.community.community.mapper;
 import life.majiang.community.community.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
 @Mapper
 public interface UserMapper {
-        @Insert(value = "insert into user (name,accont_id,token,gmt_create,gmt_modified) values(#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
-        public void insert(User user);
-
+        @Insert("insert into c_user (u_name,u_account_id,u_token,u_gmt_create,u_gmt_modified) values(#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
+        void insert(User user);
+        //类直接写，不是类，需要加@pram
+        @Select("select * from c_user where u_token = #{token}")
+        User findByToken(@Param("token") String token);
 }
